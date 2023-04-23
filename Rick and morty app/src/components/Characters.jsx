956975ae2bf2870ee88a0251232988d910caf6f2
LsrkMateo <121040035+LsrkMateo@users.noreply.github.com>
2023-04-23 17:10:04 -0500

    Add a reactive color to the card according to the state

--- Rick and morty app/src/components/Characters.jsx
+++ Rick and morty app/src/components/Characters.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Characters = ({ characters = [] }) => {
+  const estiloCardAlive = {
+    backgroundColor: "rgba(0, 128, 0, 0.5)",
+  };
+
+  const estiloCardDead = {
+    backgroundColor: "rgba(255, 0, 0, 0.5)",
+  };
+
+  const estiloCardUnknown = {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  };
   return (
     <div className="row">
       {characters.map((item, index) => (
         <div key={index} className="col mb-4">
-          <div className="card" style={{ minWidth: "200px" }}>
+          <div
+            className="card"
+            style={
+              item.status === "Alive"
+                ? estiloCardAlive
+                : item.status === "Dead"
+                ? estiloCardDead
+                : estiloCardUnknown
+            }
+          >
             <img src={item.image} alt="imagen del personaje" />
             <div className="card-body">
-              <h5 className="card-title">{item.name}</h5>
+              <h5 className="card-title">
+                {item.name} - {item.species}
+              </h5>
               <hr />
-              <p>Specie: {item.species}</p>
+              <p>Origin: {item.origin.name}</p>
               <p>Location: {item.location.name}</p>
             </div>
           </div>
