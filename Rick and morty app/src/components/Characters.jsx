961b2e27fe4f6654d1292857d2fd659cb40d992e
LsrkMateo <121040035+LsrkMateo@users.noreply.github.com>
2023-04-24 06:59:31 -0500

    Adding comments for better understanding

--- Rick and morty app/src/components/Characters.jsx
+++ Rick and morty app/src/components/Characters.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 
+// Get the characters array from main 
 const Characters = ({ characters = [] }) => {
+  
+  // Card Styles 
   const estiloCardAlive = {
     backgroundColor: "rgba(0, 128, 0, 0.5)",
   };
@@ -14,16 +17,20 @@ const Characters = ({ characters = [] }) => {
   };
   return (
     <div className="row">
+
+      // Mapping characters 
       {characters.map((item, index) => (
         <div key={index} className="col mb-4">
+          
+          // Conditional Styles 
           <div
             className="card"
             style={
-              item.status === "Alive"
-                ? estiloCardAlive
-                : item.status === "Dead"
-                ? estiloCardDead
-                : estiloCardUnknown
+              item.status === "Alive" // if
+                ? estiloCardAlive // Set
+                : item.status === "Dead" // else if
+                ? estiloCardDead // set
+                : estiloCardUnknown // else... set
             }
           >
             <img src={item.image} alt="imagen del personaje" />
